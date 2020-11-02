d17e14e21de0db79f18b8fb38313461a4231b589
Veraj Paruthi <veraj.paruthi@gmail.com>
2020-11-01 22:45:17 -0500

    Removed boilerplate code

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import mondaySdk from 'monday-sdk-js';
 
 const monday = mondaySdk();
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // Default state
-    this.state = {
-      settings: {},
-      name: '',
-    };
-  }
-
-  render() {
-    return <div className="App">Hello, monday Apps!</div>;
-  }
-}
+const App = () => (
+  <div className="App">Hello, monday Apps!</div>
+);
 
 export default App;
