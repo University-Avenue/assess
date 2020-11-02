78a1e5e31746faea7a160362d29c80b667fd4ab6
Veraj Paruthi <veraj.paruthi@gmail.com>
2020-11-01 23:15:59 -0500

    Added language state

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import AceEditor from 'react-ace';
 import 'ace-builds/src-noconflict/mode-java';
+import 'ace-builds/src-noconflict/mode-python';
+import 'ace-builds/src-noconflict/mode-c_cpp';
 import 'ace-builds/src-noconflict/theme-monokai';
 import 'ace-builds/src-noconflict/ext-language_tools';
 
 const Editor = () => {
   const [value, setValue] = useState('');
+  const [language, setLanguage] = useState('python');
 
   return (
     <AceEditor
-      mode="java"
+      mode={language}
       theme="monokai"
       fontSize={14}
       showPrintMargin
